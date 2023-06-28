964711e3f04fbea3ff05ec62e268a174fa7b5ee6
Suman Khadka <sk394@uakron.edu>
2023-06-28 13:40:52 -0400

    Add project files.

---
+++ ManageMyGhanta/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManageMyGhanta.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ManageMyGhanta/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManageMyGhanta.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+        //foreign key
+        public int Amount { get; set; }
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
