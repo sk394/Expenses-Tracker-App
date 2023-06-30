bafd9ffca92abbf9fbb88f9977e86649b6931e52
Suman Khadka <sk394@uakron.edu>
2023-06-30 00:12:11 -0400

    mid-commit

--- ManageMyGhanta/Models/Transaction.cs
+++ ManageMyGhanta/Models/Transaction.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ManageMyGhanta.Models
 {
@@ -7,8 +8,31 @@ namespace ManageMyGhanta.Models
         [Key]
         public int TransactionID { get; set; }
         //foreign key
+        [Range(1, int.MaxValue, ErrorMessage ="Please Select a Category")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0")]
         public int Amount { get; set; }
+        [Column(TypeName ="nvarchar(75)")]
         public string? Note { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitlewithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormatedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type=="Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
     }
 }
